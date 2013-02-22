b59c526eca7c7449aaabd65a5661d2b7e0054243
jtuttas <jtuttas@gmx.net>
2013-02-22 10:13:14 +0100

    Datum ein eine HTML Datei schreiben

---
+++ DatumInHtmlDatei.ps1
@@ -0,0 +1,21 @@
+﻿cls
+$d=Get-Content ("datum.html")
+$i=1
+$t=Get-Date
+foreach ($line in $d) {
+	if ($line -match "<h1>Heute ist der:") {
+		$line="<h1>Heute ist der: $t</h1>"
+	}
+	if ($i-eq 1) {
+		$line | Set-Content("datum.html")
+	}
+	else {
+		$line | Add-Content("datum.html")
+	}
+	Write-Host ("$i $line")
+	$i++
+}
+
+
+
+
