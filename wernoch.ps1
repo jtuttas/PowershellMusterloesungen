deaefe0d1f86993d51d79fc9a5f1a79314f4da28
jtuttas <jtuttas@gmx.net>
2013-03-18 14:24:55 +0100

    Kuerzer mit Invoke-WebRequest (PS3)

--- wernoch.ps1
+++ wernoch.ps1
@@ -13,5 +13,8 @@ $webClient.Headers.Add("user-agent", "Windows Powershell WebClient Header")
 #$webClient.UseDefaultCredentials = $true
 $url = 'http://api.wolframalpha.com/v2/query?appid=QLVUAK-TVH4R83V4U&input=notable%20people%20born%20on%20'+$monat+'%20'+$tag+'%20'+$jahr+'&format=plaintext'
 [XML]$alles = $webClient.DownloadString($url)
+
+## oder so
+#[xml]$b = Invoke-WebRequest "http://api.wolframalpha.com/v2/query?appid=QLVUAK-TVH4R83V4U&input=notable%20people%20born%20on%20April%2011%201968&format=plaintext"
 $name=$alles.queryresult.pod[1].subpod.InnerText
 Write-Host ("Am "+$tag+"."+$monat+"."+$jahr+" wurde auch "+$name+" gebohren")
