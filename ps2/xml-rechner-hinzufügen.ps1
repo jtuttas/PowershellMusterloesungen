e269ab36f969ffff1cb9d19c91c7e1e90118fc43
jtuttas <jtuttas@gmx.net>
2013-05-10 17:20:03 +0200

    Noch ein paar XML Sachen

---
+++ ps2/xml-rechner-hinzufügen.ps1
@@ -0,0 +1,12 @@
+﻿cls
+[xml]$a = Get-Content  ("D:\Dropbox\temp\rechnernetz.xml")
+$pc=$a.CreateElement("pc")
+$pc.SetAttribute("mac","12:33:ef:ff:a2:7a")
+$ip=$a.CreateElement("ip")
+$ip.innerText="192.168.178.7"
+$pc.AppendChild($ip)
+$name=$a.CreateElement("name")
+$name.innerText="Franks Tablet"
+$pc.AppendChild($name)
+$a.rechnernetz.AppendChild($pc)
+$a.Save("D:\Dropbox\temp\rechnernetz_neu.xml")
---
+++ ps2/xml-rechner-ändern.ps1
@@ -0,0 +1,10 @@
+﻿
+cls
+[xml]$a = Get-Content  ("D:\Dropbox\temp\rechnernetz.xml")
+$n = $a.SelectNodes("//pc")
+foreach ($p in $n) {
+    if ($p.name -eq "Franks Notebook") {
+        $p.ip="192.168.178.10"
+    }
+}
+$a.Save("D:\Dropbox\Temp\rechnernetz_neu.xml")
