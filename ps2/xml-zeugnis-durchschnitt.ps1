fea6a95a42527b93ef7f98ab9ac6c8e1f2be29ef
jtuttas <tuttas@mmbbs.de>
2013-05-11 13:16:00 +0200

    Zeugnis Noten Durchschnitt hinzugefügt

---
+++ ps2/xml-zeugnis-durchschnitt.ps1
@@ -0,0 +1,18 @@
+﻿cls
+[xml]$a = Get-Content  ("schulklasse_zeugnis.xml")
+$n = $a.SelectNodes("//pupil")
+foreach ($p in $n) {
+    $sum=0
+    $z=$p.zeugnis.FirstChild
+    
+    do {
+        if ($z.Name -ne "fehlzeiten") {
+            $sum=$sum+$z.InnerText
+        }
+        
+    }
+    while ($z=$z.NextSibling)
+    $p.zeugnis.SetAttribute("average",$sum/4)
+
+}
+$a.Save("schulklasse_zeugnis_neu.xml")
