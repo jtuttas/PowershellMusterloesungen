f519755bf3d8b58846cc855b2c196e7fedab90ef
jtuttas <tuttas@mmbbs.de>
2013-05-18 18:09:41 +0200

    umbenennen von Bildern für Medienserver

---
+++ umbennen.ps1
@@ -0,0 +1,15 @@
+﻿cls
+$pfad = 'm:\2013'
+Get-ChildItem $pfad -Recurse | 
+  ForEach-Object {
+    $n=$_.Name.TrimEnd(".jpg")
+    #$n
+    if( $n.Contains(" ") -or $n.Contains(".")) {
+      
+      $newName = $n.Replace(" ","_")
+      $newName = $newName.Replace(".","_")
+      $newName = $newName +".jpg"
+      $newName 
+      Rename-Item $_.FullName $newName
+    }
+  }
