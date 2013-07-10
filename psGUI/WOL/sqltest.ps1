b4fe0fe493969b803d4be436f288f4722e89212b
jtuttas <tuttas@mmbbs.de>
2013-07-10 12:03:09 +0200

    sqltest nun mit try und catch

---
+++ psGUI/WOL/sqltest.ps1
@@ -0,0 +1,18 @@
+﻿$ok=$false
+do {
+    try {
+        $connString = "Server=localhost;Uid=tuttas;Pwd=joerg123;database=wol";
+        # load MySQL driver and query database
+        [void][system.reflection.Assembly]::LoadFrom("MySQL.Data.dll");
+        $conn = New-Object MySql.Data.MySqlClient.MySqlConnection;
+        $conn.ConnectionString = $connString;
+        $conn.Open();
+        $ok=$true;
+    }
+    catch {
+        . $PSScriptRoot\Config.ps1
+        $ok=$false
+    }
+}
+while ($ok -eq $false)
+
--- psGUI/sqltest.ps1
+++
@@ -1,7 +0,0 @@
-﻿$connString = "Server=localhost;Uid=tuttas;Pwd=joerg123;database=wol";
-# load MySQL driver and query database
-[void][system.reflection.Assembly]::LoadFrom("MySQL.Data.dll");
-$conn = New-Object MySql.Data.MySqlClient.MySqlConnection;
-$conn.ConnectionString = $connString;
-$conn.Open();
-
