5617e4c7915fdc2ed96fba93a76e21dff5043644
jtuttas <tuttas@mmbbs.de>
2013-09-04 12:14:21 +0200

    Fehler beseitig

--- ps1/restwert2.ps1
+++ ps1/restwert2.ps1
@@ -4,11 +4,12 @@ $s=""
 for ($i=7;$i -ge 0;$i--) {
     if ($z -ge [math]::pow(2,$i)) {
         $s+="1"
+        $z=$z-[math]::pow(2,$i)
     }
     else {
         $s+="0"
     }
-    $z=$z-[math]::pow(2,$i)
+    
 }
 
 Write-Host (" Dual=$s")
