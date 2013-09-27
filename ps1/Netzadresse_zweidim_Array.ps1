3c944c3345ff3d337526152629945bd8e3a24110
jtuttas <jtuttas@gmx.net>
2013-09-27 18:09:10 +0200

    Nun auch mit Zusatzaufgabe

--- ps1/Netzadresse_zweidim_Array.ps1
+++ ps1/Netzadresse_zweidim_Array.ps1
@@ -3,33 +3,41 @@ $sub1= Read-Host ("1. Oktett Subnetzmaske")
 $sub2=Read-Host ("2. Oktett Subnetzmaske") 
 $sub3=Read-Host ("3. Oktett Subnetzmaske")
 $sub4=Read-Host ("4. Oktett Subnetzmaske")
+$netze=@{}
 $n1=$rechnerliste[0][1] -band $sub1
 $n2=$rechnerliste[0][2] -band $sub2
 $n3=$rechnerliste[0][3] -band $sub3
 $n4=$rechnerliste[0][4] -band $sub4
+$netze[""+$n1+"."+$n2+"."+$n3+"."+$n4]++
 Write-Host ("Netz Adresse von "+$rechnerliste[0][0]+" ist $n1.$n2.$n3.$n4")
 $n1=$rechnerliste[1][1] -band $sub1
 $n2=$rechnerliste[1][2] -band $sub2
 $n3=$rechnerliste[1][3] -band $sub3
 $n4=$rechnerliste[1][4] -band $sub4
+$netze[""+$n1+"."+$n2+"."+$n3+"."+$n4]++
 Write-Host ("Netz Adresse von "+$rechnerliste[1][0]+" ist $n1.$n2.$n3.$n4")
 $n1=$rechnerliste[2][1] -band $sub1
 $n2=$rechnerliste[2][2] -band $sub2
 $n3=$rechnerliste[2][3] -band $sub3
 $n4=$rechnerliste[2][4] -band $sub4
+$netze[""+$n1+"."+$n2+"."+$n3+"."+$n4]++
 Write-Host ("Netz Adresse von "+$rechnerliste[2][0]+" ist $n1.$n2.$n3.$n4")
 $n1=$rechnerliste[3][1] -band $sub1
 $n2=$rechnerliste[3][2] -band $sub2
 $n3=$rechnerliste[3][3] -band $sub3
 $n4=$rechnerliste[3][4] -band $sub4
+$netze[""+$n1+"."+$n2+"."+$n3+"."+$n4]++
 Write-Host ("Netz Adresse von "+$rechnerliste[3][0]+" ist $n1.$n2.$n3.$n4")
 $n1=$rechnerliste[4][1] -band $sub1
 $n2=$rechnerliste[4][2] -band $sub2
 $n3=$rechnerliste[4][3] -band $sub3
 $n4=$rechnerliste[4][4] -band $sub4
+$netze[""+$n1+"."+$n2+"."+$n3+"."+$n4]++
 Write-Host ("Netz Adresse von "+$rechnerliste[4][0]+" ist $n1.$n2.$n3.$n4")
 $n1=$rechnerliste[5][1] -band $sub1
 $n2=$rechnerliste[5][2] -band $sub2
 $n3=$rechnerliste[5][3] -band $sub3
 $n4=$rechnerliste[5][4] -band $sub4
+$netze[""+$n1+"."+$n2+"."+$n3+"."+$n4]++
 Write-Host ("Netz Adresse von "+$rechnerliste[5][0]+" ist $n1.$n2.$n3.$n4")
+$netze
