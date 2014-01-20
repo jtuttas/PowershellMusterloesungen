ed0248b063942ddff0ba72b360e779840ebb232d
molsondry <kemmries@mmbbs.de>
2014-01-20 23:28:09 +0100

    Musterlösung "Hintergrundfarbe"

---
+++ psGUI/hintergrundFarbe.ps1
@@ -0,0 +1,15 @@
+$myF = New-Object System.Windows.Forms.Form
+$myF.Show();
+$myF.Text="Hintergrundfarbe"
+do {
+   $a=Read-Host("q = Fenster schließen")
+   
+   if ($a -eq "r") {$myF.BackColor="red"}
+   elseif ($a -eq "g"){$myF.BackColor="green"}
+   elseif ($a -eq "b"){$myF.BackColor="blue"}
+   else {$myF.backcolor="white"}
+  
+}
+while ($a -ne "q")
+ 
+$myF.Close()
