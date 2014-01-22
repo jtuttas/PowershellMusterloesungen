9ee80c9c70c90551aa791617d750c35590891078
jtuttas <tuttas@mmbbs.de>
2014-01-22 14:48:41 +0100

    Create udpserver.ps1

---
+++ udpserver.ps1
@@ -0,0 +1,6 @@
+$port=2021
+$endpoint = new-object System.Net.IPEndPoint ([IPAddress]::Any,$port)
+$udpclient=new-Object System.Net.Sockets.UdpClient $port
+$content=$udpclient.Receive([ref]$endpoint)
+[Text.Encoding]::ASCII.GetString($content)
+$udpclient.Close()
