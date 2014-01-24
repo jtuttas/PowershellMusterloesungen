94b56993642473357434b21cdaf6be3f8c7dc096
jtuttas <jtuttas@gmx.net>
2014-01-24 17:37:10 +0100

    Ein UDP Chat Client / Server
    noch im Alpha Stadium

--- udpclient.ps1
+++ udpclient.ps1
@@ -1,5 +1,5 @@
-$port=2021
-$endpoint = new-object System.Net.IPEndPoint ([IPAddress]"10.13.6.100",$port)
+$port=2024
+$endpoint = new-object System.Net.IPEndPoint ([IPAddress]"127.0.0.1",$port)
 $udpclient=new-Object System.Net.Sockets.UdpClient
 $b=[Text.Encoding]::ASCII.GetBytes('Is anyone there!')
 $bytesSent=$udpclient.Send($b,$b.length,$endpoint)
--- udpserver.ps1
+++ udpserver.ps1
@@ -1,6 +1,47 @@
-$port=2021
+$port=2027
+$clients = @{}
 $endpoint = new-object System.Net.IPEndPoint ([IPAddress]::Any,$port)
 $udpclient=new-Object System.Net.Sockets.UdpClient $port
-$content=$udpclient.Receive([ref]$endpoint)
-[Text.Encoding]::ASCII.GetString($content)
+$udpclient.Client.ReceiveTimeout=10000
+Write-Host "Der Server läuft au Port $port" 
+do {
+    try {
+        $content=$udpclient.Receive([ref]$endpoint)
+        if ($clients.ContainsKey([String]$endpoint.Address)) {
+            Write-Host "Empfange von Client:"$endpoint.Address
+        }
+        else {    
+            Write-Host "Neue Verbindung von Client"$endpoint.Address
+            [String]$url=$endpoint.Address
+            $clients.Add($url,[System.Net.IPEndPoint]$endpoint)
+        }
+   
+        $b=[Text.Encoding]::ASCII.GetString($content)
+        Write-Host "Empfange [$b]"
+
+        foreach ($client in $clients) {
+            [String]$k=$clients.Keys[0]
+            $e=$clients[$k]
+            $txt = $endpoint.Address.ToString()+"-> "+$b
+            $b=[Text.Encoding]::ASCII.GetBytes($txt)
+            try {
+                $bytesSent=$udpclient.Send($b,$b.length,[System.Net.IPEndPoint]$e)
+                Write-Host "sende [$txt] an "$e.Address
+            }
+            catch {
+              [String]$url=$e.Address
+              $clients.Remove($url)
+              Write-Host "-Entferne Client "$e.Address
+            }
+        }
+
+    }
+    catch {
+      [String]$url=$endpoint.Address
+      $clients.Remove($url)
+      Write-Host "Entferne Client "$url
+    }
+}
+while ($true)
+
 $udpclient.Close()
