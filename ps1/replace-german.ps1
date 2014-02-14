0af47f6f13d65b193c74b418f96b82ce35f92182
jtuttas <jtuttas@gmx.net>
2014-02-14 13:43:48 +0100

    Deutsche Umlaute entfernen als Funktion undals Pipeline CMD let nutzbar

---
+++ ps1/replace-german.ps1
@@ -0,0 +1,40 @@
+﻿<#
+.Synopsis
+   Ein Beipiel CMD let
+.DESCRIPTION
+   Ein Beipiel CMD let
+.EXAMPLE
+   bsp.ps1 hallo 5
+.EXAMPLE
+   bsp.ps1 hallo
+#>
+function replace-german
+{
+    [OutputType([String])]
+    Param
+    (
+        # Das zu wiederholende Wort
+        [Parameter(Mandatory=$true,ValueFromPipeline=$true)]
+        [string[]]$file        
+    )
+
+    Begin
+    {
+    }
+    Process
+    {        
+        foreach ($s in $file) {
+            $s=$s.replace("ö","oe");
+            $s=$s.replace("Ö","OE");
+            $s=$s.replace("ä","ae");
+            $s=$s.replace("Ä","AE");
+            $s=$s.replace("ü","ue");
+            $s=$s.replace("Ü","UE");
+            $s=$s.replace("ß","ss");
+            return $s
+        }
+    }
+    End
+    {
+    }
+}
