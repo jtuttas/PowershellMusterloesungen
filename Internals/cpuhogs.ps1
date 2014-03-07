99b4869cd8e9270daea614dfab5b60e00b398464
jtuttas <jtuttas@gmx.net>
2014-03-07 12:16:18 +0100

    Einige Remote Aufgaben

---
+++ Internals/cpuhogs.ps1
@@ -0,0 +1,6 @@
+﻿$comps = Import-Csv "$Home\computer.csv"
+foreach ($c in $comps) {
+    $pw= ConvertTo-SecureString $c.Password -AsPlainText -Force
+    $cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $c.User, $pw
+    Invoke-Command -ComputerName $c.ComputerName -Credential $cred -Command {Get-Process | Sort-Object -Property cpu -descending | Select-Object -First 3}
+}
---
+++ Internals/filehogs.ps1
@@ -0,0 +1,5 @@
+﻿$hogs= {
+    cd $HOME
+    $f=Get-ChildItem -Recurse -File | Sort-Object -Property Length -Descending | Select-Object -First 3 
+    $f
+}
