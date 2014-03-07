9cf1186fee0c47aef553a2cf6fa46c855a1f1d9f
jtuttas <jtuttas@gmx.net>
2014-03-07 16:32:06 +0100

    cpu-load via WMI

---
+++ Internals/cpu-load.ps1
@@ -0,0 +1,17 @@
+﻿param (
+    [int]$dt,
+    [int]$count
+)
+
+function cpu-load {
+    $p = Get-WmiObject win32_processor
+    $load = 'load' | Select-Object -Property Timestamp, Load
+    $load.Timestamp = Get-Date
+    $load.Load = $p.LoadPercentage
+    $load
+}
+
+for ($i=0;$i -lt $count;$i++) {
+    cpu-load
+    sleep $dt
+}
