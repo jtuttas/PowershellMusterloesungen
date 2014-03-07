7ee9b6d901f2e92299c0970758042fb3fc9b9171
jtuttas <jtuttas@gmx.net>
2014-03-07 14:40:20 +0100

    get-product liste der installierten software

---
+++ Internals/get-product.ps1
@@ -0,0 +1,13 @@
+﻿function get-product {
+    $prod = Get-WmiObject win32_product
+    foreach ($p in $prod) {
+
+        $product = 'product' | Select-Object -Property Name, Date
+        $product.Name = $p.Name
+        $product.Date = $p.InstallDate
+        $product
+
+    }
+}
+
+get-product | Sort-Object -Property Name | Out-GridView
