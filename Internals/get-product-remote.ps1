ed6b0bfa7e93c079ea25baee5f933fddddbfb2e0
jtuttas <jtuttas@gmx.net>
2014-03-07 17:25:50 +0100

    Softwareinventarisierung remote

---
+++ Internals/get-product-remote.ps1
@@ -0,0 +1,12 @@
+﻿$comps = Import-Csv "$Home\computer.csv"
+foreach ($c in $comps) {
+    $pw= ConvertTo-SecureString $c.Password -AsPlainText -Force
+    $cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $c.User, $pw
+    $prod =Get-WmiObject win32_product -ComputerName $c.ComputerName -Credential $cred
+    foreach ($p in $prod) {
+        $product = 'product' | Select-Object -Property Name, Date
+        $product.Name = $p.Name
+        $product.Date = $p.InstallDate
+        $product
+    }
+}
