c849387497d9bbe5bcca58aa639d3da8e2198a50
jtuttas <tuttas@mmbbs.de>
2014-03-13 17:50:43 +0100

    Add multiple AD users

---
+++ Internals/AddmultipleUsers.ps1
@@ -0,0 +1,7 @@
+﻿$csv = Import-Csv "$HOME\ADbenutzer.csv"
+foreach ($benutzer in $csv) {
+    $pw = ConvertTo-SecureString -AsPlainText $benutzer.Kennwort  -Force
+    $mail = $benutzer.Name+"@tuttas.de"
+    New-ADUser -Name ($benutzer.Name) -Surname ($benutzer.Name) -GivenName ($benutzer.Vorname) -UserPrincipalName $mail -Enabled $true -AccountPassword  $pw
+    Write-Host "Benutzer "$benutzer.Vorname" "$benutzer.Name" angelegt"
+}
