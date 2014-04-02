a1d445254cf77a487412a1e42fc03ff7579f1790
jtuttas <jtuttas@gmx.net>
2014-04-02 09:41:00 +0200

    Write-SmartState

---
+++ Internals/write-smartstate.ps1
@@ -0,0 +1,9 @@
+﻿function Write-SmartState {
+    param (
+        [Parameter(Mandatory=$true)]
+        [String] $file
+    )
+
+    $w=(Get-WMIObject Win32_DiskDrive | Select-Object Status,Model,Date | ForEach-Object {$_.Date=(get-Date);$_}) | Export-Csv -NoTypeInformation -Path $file -Append
+    
+}
