bac9b327735a8362738cc8891e9ba8154982bfc6
jtuttas <tuttas@mmbbs.de>
2015-03-10 11:53:29 +0100

    Kennwort ändern und Mail versenden via Powershell

---
+++ sonst/change_password.ps1
@@ -0,0 +1,24 @@
+﻿param($kennwort)
+$script={
+param($pw)
+try {
+  $user=Get-ADUser -Filter {EmailAddress -like "jtuttas@gmx.net"} -Properties EmailAddress 
+  $newpwd=ConvertTo-SecureString -String $pw -AsPlainText -Force
+  Set-ADAccountPassword $user -NewPassword $newpwd -Reset -PassThru | Set-ADUser -ChangePasswordAtLogon $true
+  return "Kennwort geändert zu $pw"
+}
+catch  {
+  return "Es ist ein Fehler aufgetreten"
+}
+}
+$username="Administrator"
+      if (Test-Path C:\Temp\admin.txt) {
+        Write-Host "Found Secure String"
+        $password = Get-Content C:\Temp\admin.txt | convertto-securestring
+        $cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $username, $password
+      }
+      else {
+            $cred = Get-Credential -UserName $username -Message "Kennwort f. Administrator"
+            ConvertFrom-SecureString $cred.Password | Set-Content C:\Temp\admin.txt
+      }
+Invoke-Command -ComputerName 10.1.100.0 -Credential $cred -ScriptBlock $script -ArgumentList $kennwort
---
+++ sonst/send_request.ps1
@@ -0,0 +1,56 @@
+﻿param ($id,$mail)
+
+<#
+.Synopsis
+   Kurzbeschreibung
+.DESCRIPTION
+   Lange Beschreibung
+.EXAMPLE
+   Beispiel für die Verwendung dieses Cmdlets
+.EXAMPLE
+   Ein weiteres Beispiel für die Verwendung dieses Cmdlets
+#>
+function send-request
+{
+    [OutputType([String])]
+    Param
+    (
+        # Hilfebeschreibung zu Param1
+        [Parameter(Mandatory=$true,
+                   Position=0)]
+        [String]$id,
+
+        # Hilfebeschreibung zu Param2
+        [Parameter(Mandatory=$true,
+                   Position=1)]
+        [String]$email
+    )
+
+    Begin
+    {
+      $username="tuttas68@googlemail.com"
+      if (Test-Path C:\Temp\serurestring.txt) {
+        Write-Host "Found Secure String"
+        $password = Get-Content C:\Temp\serurestring.txt | convertto-securestring
+        $cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $username, $password
+      }
+      else {
+            $cred = Get-Credential -UserName $username -Message "Kennwort f. Mailserver"
+            ConvertFrom-SecureString $cred.Password | Set-Content C:\Temp\serurestring.txt
+      }
+      
+      $SMTPServer="smtp.gmail.com"
+      $SMTPPort="587"
+      $body=" Bitte Klicken Sie auf den Link http://localhost/sql.php?id="+$id;
+      Send-MailMessage -From "tuttas@mmbbs.de" -to $email -Subject "Kennwort Wiederherstellung" -Body $Body -SmtpServer $SMTPServer -port $SMTPPort -UseSsl -Credential $cred
+    }
+    Process
+    {
+    }
+    End
+    {
+        return "send mail to $email"
+    }
+}
+
+send-request $id $mail
