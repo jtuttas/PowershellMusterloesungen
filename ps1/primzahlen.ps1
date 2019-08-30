0a9f36e99cd22a7eba5bf828e15364c846252d6f
Ch4r0ne <54702161+Ch4r0ne@users.noreply.github.com>
2019-08-30 13:40:23 +0200

    Update primzahlen.ps1

--- ps1/primzahlen.ps1
+++ ps1/primzahlen.ps1
@@ -1,4 +1,56 @@
-﻿cls
+#Option 1
+
+$input1=Read-Host "Please input a number"
+for ($i=2; $i-le $input1; $i++) {
+    $found=$true;
+    for ($j=2; $j -lt $i; $j++) {
+        if (($i % $j) -eq 0) {
+            $found = $false;
+            break;
+        }
+    }
+    if ($found) {
+        Write-Host $i;
+    }
+}
+
+
+#Option 2
+
+$input1=Read-Host "Please input a number"
+for ($i=2; $i-le $input1; $i++) {
+    $found=$true;
+    for ($j=2; $j -lt $i; $j++) {
+        if (([int]($i/$j))*$j -eq $i) {
+            $found = $false;
+            break;
+        }
+    }
+    if ($found) {
+        Write-Host $i;
+    }
+}
+
+
+#Option 3
+
+$input1=Read-Host "Please input a number"
+for ($i=2; $i-le $input1; $i++) {
+    $found=$true;
+    for ($j=2; $j -lt $i; $j++) {
+        if (($i/$j) -is [int]) {
+            $found = $false;
+            break;
+        }
+    }
+    if ($found) {
+        Write-Host $i;
+    }
+}
+
+# Option 4
+
+cls
 # Primzahlen bestimmung mit der Powershell
 $n=0;
 $t=0;
